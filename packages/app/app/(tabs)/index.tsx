11b04b5a127a247f612bae76d90d8f9e5ae794ec
Ugochukwu Chukwuma <0xUgochukwu@gmail.com>
2025-03-17 21:57:03 +0000

    moving

--- packages/app/app/(tabs)/index.tsx
+++ packages/app/app/(tabs)/index.tsx
@@ -5,27 +5,27 @@ import { Text, View } from '@/components/Themed';
 
 export default function TodayScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Today</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    <View className='flex-1 items-center justify-center'>
+      <Text className='text-2xl font-bold' >Today</Text>
+      <View lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/today.tsx" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
